b93a1e211a01aa01400626800e9b3f1e3ef3c260
GayatriDewade <gayatridevade31@gmail.com>
2026-05-25 20:57:58 +0530

    Exception and Namespaces

---
+++ 13.Exception_Handling_17May/test.py
@@ -0,0 +1,19 @@
+# test=['5','0','abc']
+
+# for simulated_input in test:
+#     print(f"\n Testing with input : '{simulated_input}'")
+
+#     try:
+#         user_input=simulated_input
+#         number=int(user_input)
+#         result=100/number
+
+#     except ValueError as err:
+#         print("ValueError : ",err)
+
+#     except ZeroDivisionError as err:
+#         print("ZeroDivisionError : ",err)
+
+#     else:
+#         print("Result is successful")
+
